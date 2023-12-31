3edbf0b001198afe38b84e022e93ade1f468d791
olepak <oleksandr.lepak@gmail.com>
2023-12-31 13:53:35 +0200

    upd

--- math-methods.js
+++ math-methods.js
@@ -1,7 +1,26 @@
 // full article - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math
-// here are the most used
+// here are the most useful
 
 // Math.E represents Euler's number 2.781
 console.log(Math.E);
 let mathE = 3 * Math.E;
 console.log(mathE);
+
+// Math.PI represents 3.1415
+console.log(Math.PI);
+let mathPi = 3 * Math.PI;
+console.log(mathPi);
+
+// Math.abs() returns the absolute value of a number. It is useful when there is a need to convert negative to a positive.
+console.log(Math.abs(3 - 5));
+console.log(Math.abs(5 - 3));
+console.log(Math.abs(-3));
+
+// Math.cbrt() returns the cube root of a number
+console.log(Math.cbrt(27));
+console.log(Math.cbrt(64));
+
+// Math.ceil() rounds up and returns the smallest integer greater than of equal to a given number
+console.log(Math.ceil(7));
+console.log(Math.ceil(7.003));
+console.log(Math.ceil(-7.4));
