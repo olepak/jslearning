68e1775f935fd5d2072ccee191f08099865c5b22
olepak <oleksandr.lepak@gmail.com>
2023-12-31 13:36:04 +0200

    create math-methods.js

---
+++ math-methods.js
@@ -0,0 +1,7 @@
+// full article - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math
+// here are the most used
+
+// Math.E represents Euler's number 2.781
+console.log(Math.E);
+let mathE = 3 * Math.E;
+console.log(mathE);
