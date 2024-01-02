7be75570936f754b67cef2243cd3fc005a01a908
olepak <oleksandr.lepak@gmail.com>
2024-01-02 16:50:31 +0200

    upd

--- math-methods.js
+++ math-methods.js
@@ -37,3 +37,26 @@ console.log(Math.max(...maxArray));
 // Math.min() returns the smallest of the numbers given as input parameters, or Infinity if there are no parameters
 const minArray = [12, 354, 5, 45, 643, 5, 15, 4];
 console.log(Math.min(...minArray));
+
+// Math.pow() returns the value of a base raised to a power
+console.log(Math.pow(7, 3));
+// where 7 is a base, 3 is a power
+
+// Math.random() return a floating-point, pseudo-random number that's greater than or equal to 0 and less than 1, with approximately uniform distribution over the range. It does not provide secure random numbers.
+console.log(Math.random());
+console.log(Math.random() * 10);
+// getting a random value between two numbers
+function getRandomArbitrary(min, max) {
+	return Math.random() * (max - min) + min;
+}
+console.log(getRandomArbitrary(4, 10));
+// getting a random integer between two values ( max exclusive) https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random#getting_a_random_integer_between_two_values
+
+// Math.round returns the value of a number rounded to the nearest integer
+console.log(Math.round(5.49));
+console.log(Math.round(5.51));
+
+// Math.sign()returns 1 ir -1, indicating the sign if the number passed as argument. If the input is 0 or -0 it will be returned as is.
+console.log(Math.sign(3));
+console.log(Math.sign(-3));
+console.log(Math.sign(0));
